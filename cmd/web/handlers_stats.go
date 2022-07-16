5fd5c263c1a2baa04d52c188e5e7201e13054358
Vladimir Simakhin <vladimir.simakhin@accolade.com>
2022-07-16 18:39:03 +0200

    add chart section for stats

--- cmd/web/handlers_stats.go
+++ cmd/web/handlers_stats.go
@@ -79,7 +79,7 @@ func (app *application) HandlerStats(w http.ResponseWriter, r *http.Request) {
 	data["totalsByAircraft"] = totalsByAircraft
 	data["totalsByClass"] = totalsByClass
 
-	partials := []string{"stats-totals", "stats-totals-year", "stats-totals-type", "stats-totals-class"}
+	partials := []string{"stats-totals", "stats-totals-year", "stats-totals-type", "stats-totals-class", "stats-js"}
 
 	if err := app.renderTemplate(w, r, "stats", &templateData{Data: data}, partials...); err != nil {
 		app.errorLog.Println(err)
