87ef48847a9cc54f15f88de365991db025c1e9fb
Vladimir Simakhin <vladimir.simakhin@accolade.com>
2022-10-23 13:35:20 +0200

    check for new released version

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 // writeJSON writes arbitrary data out as JSON
@@ -27,4 +30,36 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 	}
 
 	return nil
+}
+
+// checkNewVersion checks if the new version released on github
+func (app *application) checkNewVersion() {
+	type githubResponse struct {
+		Name string `json:"name"`
+	}
+
+	resp, err := http.Get("https://api.github.com/repos/vsimakhin/web-logbook/releases/latest")
+	if err != nil {
+		app.infoLog.Println(fmt.Errorf("cannot retrieve the latest release, no internet connection? - %s", err))
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		app.errorLog.Println(fmt.Errorf("cannot retrieve the latest release - %s", err))
+		return
+	}
+
+	var release githubResponse
+	err = json.Unmarshal(body, &release)
+	if err != nil {
+		app.errorLog.Println(fmt.Errorf("error decoding github response - %s", err))
+		return
+	}
+
+	// just a simple compare of the versions, if not equal - show the badge
+	if !strings.Contains(release.Name, app.version) {
+		app.infoLog.Printf("new version %s is available https://github.com/vsimakhin/web-logbook\n", release.Name)
+		app.isNewVersion = true
+	}
 }
