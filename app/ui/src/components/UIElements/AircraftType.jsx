6a38a670b4b84b16f576a968d19726bdaa1d766c
Daniel Kappelle <daniel.kappelle@gmail.com>
2025-05-14 09:27:37 +0200

    Fix bug with aircraft type autocomplete box if there are no records yet

--- app/ui/src/components/UIElements/AircraftType.jsx
+++ app/ui/src/components/UIElements/AircraftType.jsx
@@ -1,32 +1,40 @@
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 // Custom components
-import Select from "../UIElements/Select"
+import Select from "../UIElements/Select";
 import { fetchAircraftModels } from "../../util/http/aircraft";
 
-export const AircraftType = ({ gsize, id = "aircraft.model", label = "Aircraft Type", value, handleChange, ...props }) => {
+export const AircraftType = ({
+  gsize,
+  id = "aircraft.model",
+  label = "Aircraft Type",
+  value,
+  handleChange,
+  ...props
+}) => {
   const navigate = useNavigate();
 
   const { data: options = [] } = useQuery({
     queryFn: ({ signal }) => fetchAircraftModels({ signal, navigate }),
-    queryKey: ['aircraft-models'],
+    queryKey: ["aircraft-models"],
     staleTime: 3600000,
     gcTime: 3600000,
-  })
+  });
 
   return (
-    <Select gsize={gsize}
+    <Select
+      gsize={gsize}
       id={id}
       label={label}
       handleChange={handleChange}
       onBlur={(e) => handleChange(id, e.target.value)}
       value={value}
       tooltip={"Aircraft type"}
-      options={options}
+      options={options || []}
       freeSolo={true}
       {...props}
     />
   );
-}
+};
 
 export default AircraftType;
