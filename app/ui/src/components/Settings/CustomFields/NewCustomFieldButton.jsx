b77e11026d298ae40888e33d216f99b57bf324bb
Vladimir Simakhin <vsimakhin@gmail.com>
2025-05-30 16:59:36 +0200

    add routes and modal window to add/edit custom fields

---
+++ app/ui/src/components/Settings/CustomFields/NewCustomFieldButton.jsx
@@ -0,0 +1,28 @@
+import { useDialogs } from '@toolpad/core/useDialogs';
+import { memo, useCallback } from 'react';
+// MUI Icons
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+// MUI UI elements
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+// Custom components and libraries
+import { CUSTOM_FIELD_INITIAL_STATE } from '../../../constants/constants';
+import CustomField from './CustomFieldModal';
+
+export const NewCustomFieldButton = memo(() => {
+  const dialogs = useDialogs();
+
+  const handleClick = useCallback(async () => {
+    await dialogs.open(CustomField, CUSTOM_FIELD_INITIAL_STATE)
+  }, [dialogs]);
+
+  return (
+    <Tooltip title="New Custom Field">
+      <IconButton onClick={handleClick}>
+        <AddBoxOutlinedIcon fontSize='small' />
+      </IconButton>
+    </Tooltip >
+  );
+});
+
+export default NewCustomFieldButton;
--- app/ui/src/util/http/fields.jsx
+++ app/ui/src/util/http/fields.jsx
@@ -11,3 +11,47 @@ export const fetchCustomFields = async ({ signal, navigate }) => {
   };
   return await handleFetch(url, options, navigate, 'Cannot fetch custom fields');
 }
+
+export const fetchCustomField = async ({ uuid, signal, navigate }) => {
+  const url = `${API_URL}/custom-fields/${uuid}`;
+  const options = {
+    method: 'GET',
+    headers: { 'Authorization': `Bearer ${getAuthToken()}` },
+    signal: signal,
+  };
+  return await handleFetch(url, options, navigate, 'Cannot fetch custom field');
+}
+
+export const createCustomField = async ({ field, navigate }) => {
+  const url = `${API_URL}/custom-fields/new`;
+
+  const options = {
+    method: 'POST',
+    headers: { 'Authorization': `Bearer ${getAuthToken()}`, 'Content-Type': 'application/json' },
+    body: JSON.stringify(field),
+  };
+  return await handleFetch(url, options, navigate, 'Cannot create new custom field');
+}
+
+export const updateCustomField = async ({ field, navigate }) => {
+  const url = `${API_URL}/custom-fields/${field.uuid}`;
+
+  field.time_frame.value = parseInt(field.time_frame.value) || 0;
+  field.target_value = parseInt(field.target_value) || 0;
+
+  const options = {
+    method: 'PUT',
+    headers: { 'Authorization': `Bearer ${getAuthToken()}`, 'Content-Type': 'application/json' },
+    body: JSON.stringify(field),
+  };
+  return await handleFetch(url, options, navigate, 'Cannot update custom field');
+}
+
+export const deleteCustomField = async ({ uuid, navigate }) => {
+  const url = `${API_URL}/custom-fields/${uuid}`;
+  const options = {
+    method: 'DELETE',
+    headers: { 'Authorization': `Bearer ${getAuthToken()}` },
+  };
+  return await handleFetch(url, options, navigate, 'Cannot delete custom field');
+}
