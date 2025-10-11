1ab22b4ae5d5a36406448006018b34a44be048c8
Vladimir Simakhin <vsimakhin@gmail.com>
2025-10-11 15:58:09 +0200

    use warning period in the licensing table

--- app/ui/src/components/Licensing/LicensingTable.jsx
+++ app/ui/src/components/Licensing/LicensingTable.jsx
@@ -10,12 +10,13 @@ import Box from '@mui/material/Box';
 import Typography from "@mui/material/Typography";
 // Custom components and libraries
 import NewLicenseRecordButton from './NewLicenseRecordButton';
-import { calculateExpiry, createDateColumn, getExpireColor } from "./helpers";
+import { calculateExpiry, createDateColumn } from "./helpers";
 import { defaultColumnFilterTextFieldProps, tableJSONCodec } from '../../constants/constants';
 import { dateFilterFn } from '../../util/helpers';
 import CSVExportButton from '../UIElements/CSVExportButton';
 import TableFilterDrawer from '../UIElements/TableFilterDrawer';
 import ResetColumnSizingButton from '../UIElements/ResetColumnSizingButton';
+import useSettings from '../../hooks/useSettings';
 
 const paginationKey = 'licensing-table-page-size';
 const columnVisibilityKey = 'licensing-table-column-visibility';
@@ -51,6 +52,15 @@ export const LisencingTable = ({ data, isLoading }) => {
   const [pagination, setPagination] = useLocalStorageState(paginationKey, { pageIndex: 0, pageSize: 15 }, { codec: tableJSONCodec });
   const [columnSizing, setColumnSizing] = useLocalStorageState(columnSizingKey, {}, { codec: tableJSONCodec });
   const filterFns = useMemo(() => ({ dateFilterFn: dateFilterFn }), []);
+  const { settings, isLoading: isSettingsLoading } = useSettings();
+
+  const getExpireColor = useCallback((days) => {
+    const warning = settings?.licenses_expiration?.warning_period || 90;
+
+    if (days < 0) return 'error';
+    if (days < warning) return 'warning';
+    return 'inherit';
+  }, [settings]);
 
   const columns = useMemo(() => [
     { accessorKey: "category", header: "Category", size: 150 },
@@ -86,7 +96,7 @@ export const LisencingTable = ({ data, isLoading }) => {
       size: 150,
     },
     { accessorKey: "remarks", header: "Remarks", grow: true },
-  ], []);
+  ], [isSettingsLoading, getExpireColor]);
 
   const renderTopToolbarCustomActions = useCallback(({ table }) => (
     <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
