b86ef6a8f886b4217888f15f90bad76a93c83afb
Vladimir Simakhin <vsimakhin@gmail.com>
2025-10-11 13:38:32 +0200

    restructure general settings components

---
+++ app/ui/src/components/Settings/GeneralSettings/GeneralSettings.jsx
@@ -0,0 +1,33 @@
+import { memo } from "react";
+// MUI UI elements
+import Divider from "@mui/material/Divider";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+// Custom
+import CardHeader from "../../UIElements/CardHeader";
+import SaveSettingsButton from "../SaveSettingsButton";
+import OwnerInfoFields from "./OwnerInfoFields";
+import OtherSettings from "./OtherSettings";
+import AuthSettings from "./AuthSettings";
+
+const ActionButtons = memo(({ settings }) => (<SaveSettingsButton settings={settings} />));
+
+export const GeneralSettings = ({ settings, handleChange }) => {
+
+  return (
+    <Card variant="outlined" sx={{ mb: 1 }}>
+      <CardContent>
+        <CardHeader title="Settings" action={<ActionButtons settings={settings} />} />
+
+        <OwnerInfoFields settings={settings} handleChange={handleChange} />
+        <Divider sx={{ m: 1 }} />
+        <OtherSettings settings={settings} handleChange={handleChange} />
+        <Divider sx={{ m: 1 }} />
+        <AuthSettings settings={settings} handleChange={handleChange} />
+
+      </CardContent>
+    </Card>
+  );
+};
+
+export default GeneralSettings;
