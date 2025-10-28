0723deec9640565802cbc8f2d17782f55b023152
Vladimir Simakhin <vsimakhin@gmail.com>
2025-10-28 11:13:59 +0100

    add leon profile mapping

--- app/ui/src/components/Import/MapFieldsDialog.jsx
+++ app/ui/src/components/Import/MapFieldsDialog.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 // MUI UI elements
 import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
@@ -6,93 +6,117 @@ import Dialog from '@mui/material/Dialog';
 import Grid from '@mui/material/Grid2';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
+import Divider from "@mui/material/Divider";
 // MUI Icons
 import DisabledByDefaultOutlinedIcon from '@mui/icons-material/DisabledByDefaultOutlined';
 import AirplaneTicketOutlinedIcon from '@mui/icons-material/AirplaneTicketOutlined';
 import DoubleArrowOutlinedIcon from '@mui/icons-material/DoubleArrowOutlined';
+import PetsIcon from '@mui/icons-material/Pets';
 // Custom components
 import Select from '../UIElements/Select';
 import CardHeader from "../UIElements/CardHeader";
 
+const getHeader = (key, payload) => (payload.includes(key) ? key : "");
+
 const fields = [
-  { id: 'date', label: 'Date', default: 'Date' },
-  { id: 'departure_place', label: 'Departure Place', default: 'Departure Place' },
-  { id: 'departure_time', label: 'Departure Time', default: 'Departure Time' },
-  { id: 'arrival_place', label: 'Arrival Place', default: 'Arrival Place' },
-  { id: 'arrival_time', label: 'Arrival Time', default: 'Arrival Time' },
-  { id: 'aircraft_model', label: 'Aircraft Model', default: 'Aircraft Model' },
-  { id: 'aircraft_reg_name', label: 'Aircraft Reg Name', default: 'Aircraft Reg' },
+  { id: 'date', label: 'Date', default: 'Date', leon: 'flightDate' },
+  { id: 'departure_place', label: 'Departure Place', default: 'Departure Place', leon: 'from' },
+  { id: 'departure_time', label: 'Departure Time', default: 'Departure Time', leon: 'actualDepartureTime' },
+  { id: 'arrival_place', label: 'Arrival Place', default: 'Arrival Place', leon: 'to' },
+  { id: 'arrival_time', label: 'Arrival Time', default: 'Arrival Time', leon: 'actualArrivalTime' },
+  { id: 'aircraft_model', label: 'Aircraft Model', default: 'Aircraft Model', leon: 'selectedAircraftType' },
+  { id: 'aircraft_reg_name', label: 'Aircraft Reg Name', default: 'Aircraft Reg', leon: 'selectedAircraftID' },
   { id: 'se_time', label: 'SE Time', default: 'Time SE' },
   { id: 'me_time', label: 'ME Time', default: 'Time ME' },
-  { id: 'mcc_time', label: 'MCC Time', default: 'Time MCC' },
-  { id: 'total_time', label: 'Total Time', default: 'Time Total' },
-  { id: 'night_time', label: 'Night Time', default: 'Time Night' },
-  { id: 'ifr_time', label: 'IFR Time', default: 'Time IFR' },
-  { id: 'pic_time', label: 'PIC Time', default: 'Time PIC' },
-  { id: 'co_pilot_time', label: 'Co Pilot Time', default: 'Time CoPilot' },
-  { id: 'dual_time', label: 'Dual Time', default: 'Time Dual' },
-  { id: 'instructor_time', label: 'Instructor Time', default: 'Time Instructor' },
-  { id: 'landings_day', label: 'Landings Day', default: 'Landings Day' },
-  { id: 'landings_night', label: 'Landings Night', default: 'Landings Night' },
+  { id: 'mcc_time', label: 'MCC Time', default: 'Time MCC', leon: 'multiPilot' },
+  { id: 'total_time', label: 'Total Time', default: 'Time Total', leon: 'totalTime' },
+  { id: 'night_time', label: 'Night Time', default: 'Time Night', leon: 'night' },
+  { id: 'ifr_time', label: 'IFR Time', default: 'Time IFR', leon: 'ifr' },
+  { id: 'pic_time', label: 'PIC Time', default: 'Time PIC', leon: 'pic' },
+  { id: 'co_pilot_time', label: 'Co Pilot Time', default: 'Time CoPilot', leon: 'sic' },
+  { id: 'dual_time', label: 'Dual Time', default: 'Time Dual', leon: 'dualReceived' },
+  { id: 'instructor_time', label: 'Instructor Time', default: 'Time Instructor', leon: 'dualGiven' },
+  { id: 'landings_day', label: 'Landings Day', default: 'Landings Day', leon: 'dayLandings' },
+  { id: 'landings_night', label: 'Landings Night', default: 'Landings Night', leon: 'nightLandings' },
   { id: 'sim_type', label: 'Sim Type', default: 'SIM Type' },
   { id: 'sim_time', label: 'Sim Time', default: 'SIM Time' },
-  { id: 'pic_name', label: 'PIC Name', default: 'PIC Name' },
+  { id: 'pic_name', label: 'PIC Name', default: 'PIC Name', leon: 'selectedCrewCommander' },
   { id: 'remarks', label: 'Remarks', default: 'Remarks' },
 ];
 
+const generateProfile = (payload, fieldKey = "default") => {
+  const profile = {};
+  for (const field of fields) {
+    const key = field[fieldKey];
+    const header = getHeader(key, payload);
+    if (header) profile[field.id] = header;
+  }
+  return profile;
+};
+
+const ProfileButton = ({ tooltip, icon: Icon, fieldKey, setProfile, payload }) => {
+  const handleClick = useCallback(() => {
+    setProfile(generateProfile(payload, fieldKey));
+  }, [payload, setProfile, fieldKey]);
+
+  return (
+    <Tooltip title={tooltip}>
+      <IconButton size="small" onClick={handleClick}><Icon /></IconButton>
+    </Tooltip>
+  );
+}
+
 const MapFieldsDialog = ({ open, onClose, payload }) => {
-  const [result, setResult] = useState({});
+  const [profile, setProfile] = useState({});
+  const handleChange = useCallback((key, value) => { setProfile((prev) => ({ ...prev, [key]: value })) }, [setProfile]);
 
-  const handleChange = (key, value) => { setResult((prev) => ({ ...prev, [key]: value })) };
-
-  const getHeader = (key) => { return payload.includes(key) ? key : "" };
-
-  const loadLogbookProfile = () => {
-    const newResult = {};
-
-    for (const field of fields) {
-      const header = getHeader(field.default);
-      if (header) {
-        newResult[field.id] = header;
-      }
-    }
-
-    setResult(newResult);
-  };
+  const actionButtons = useMemo(() => (
+    <Box display="flex" alignItems="center" gap={0}>
+      <ProfileButton tooltip="Apply Leon Mapping"
+        fieldKey="leon"
+        icon={PetsIcon}
+        setProfile={setProfile} payload={payload}
+      />
+      <ProfileButton tooltip="Apply Web Logbook Mapping"
+        fieldKey="default"
+        icon={AirplaneTicketOutlinedIcon}
+        setProfile={setProfile} payload={payload}
+      />
+      <Divider orientation="vertical" flexItem sx={{ mx: 0.5 }} />
+      <Tooltip title="Continue">
+        <IconButton size="small" onClick={() => onClose(profile)} disabled={Object.keys(profile).length === 0}>
+          <DoubleArrowOutlinedIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Close">
+        <IconButton size="small" onClick={onClose}>
+          <DisabledByDefaultOutlinedIcon />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  ), [onClose, profile, setProfile, payload]);
 
   return (
     <Dialog fullWidth open={open} onClose={() => onClose(null)}>
       <Card variant="outlined" sx={{ m: 2 }}>
         <CardContent>
-          <CardHeader title="Map Fields"
-            action={
-              <>
-                <Tooltip title="Apply Web Logbook Standard Mapping">
-                  <IconButton size="small" onClick={() => loadLogbookProfile()}><AirplaneTicketOutlinedIcon /></IconButton>
-                </Tooltip>
-                <Tooltip title="Continue">
-                  <IconButton size="small" onClick={() => onClose(result)}><DoubleArrowOutlinedIcon /></IconButton>
-                </Tooltip>
-                <Tooltip title="Close">
-                  <IconButton size="small" onClick={() => onClose()}><DisabledByDefaultOutlinedIcon /></IconButton>
-                </Tooltip>
-              </>
-            }
-          />
+          <CardHeader title="Map Fields" action={actionButtons} />
           <Grid container spacing={1}>
             {fields.map((field, index) => (
-              <Select gsize={{ xs: 12, sm: 6, md: 6, lg: 6, xl: 6 }} key={index}
+              <Select gsize={{ xs: 12, sm: 6, md: 6, lg: 6, xl: 6 }}
+                key={field.id}
                 id={field.id}
                 label={field.label}
                 handleChange={handleChange}
                 options={payload}
-                value={result[field.id] ?? ""}
+                value={profile[field.id] ?? ""}
                 disableClearable={false}
               />
             ))}
           </Grid>
         </CardContent>
-      </Card >
+      </Card>
     </Dialog>
   );
 }
