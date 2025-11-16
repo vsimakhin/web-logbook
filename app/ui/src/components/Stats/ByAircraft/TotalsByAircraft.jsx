a71613e076dbe304f7074fdfac40eb316a369bd6
Vladimir Simakhin <vsimakhin@gmail.com>
2025-11-16 20:07:07 +0100

    update stats by category

--- app/ui/src/components/Stats/ByAircraft/TotalsByAircraft.jsx
+++ app/ui/src/components/Stats/ByAircraft/TotalsByAircraft.jsx
@@ -5,13 +5,13 @@ import { useErrorNotification } from "../../../hooks/useAppNotifications";
 import { fetchLogbookData } from "../../../util/http/logbook";
 import { getTotalsByAircraft } from "../../../util/helpers";
 import TotalsByAircraftTable from "./TotalsByAircraftTable";
-import { fetchAircraftModelsCategories } from "../../../util/http/aircraft";
+import { fetchAircraftModelsCategories, fetchAircrafts } from "../../../util/http/aircraft";
 import useCustomFields from "../../../hooks/useCustomFields";
 
 export const TotalsByAircraft = ({ type }) => {
   const navigate = useNavigate();
 
-  const { data, isLoading, isError, error } = useQuery({
+  const { data: flights = [], isLoading, isError, error } = useQuery({
     queryKey: ['logbook'],
     queryFn: ({ signal }) => fetchLogbookData({ signal, navigate }),
     staleTime: 3600000,
@@ -19,17 +19,26 @@ export const TotalsByAircraft = ({ type }) => {
   });
   useErrorNotification({ isError, error, fallbackMessage: 'Failed to load logbook' });
 
-  const { data: models } = useQuery({
+  const { data: models = [] } = useQuery({
     queryKey: ['models-categories'],
     queryFn: ({ signal }) => fetchAircraftModelsCategories({ signal, navigate }),
+    staleTime: 3600000,
+    gcTime: 3600000,
   });
+
+  const { data: aircrafts = [] } = useQuery({
+    queryKey: ['aircrafts'],
+    queryFn: ({ signal }) => fetchAircrafts({ signal, navigate }),
+    staleTime: 3600000,
+    gcTime: 3600000,
+  })
 
   const { customFields } = useCustomFields();
 
   return (
     <TotalsByAircraftTable
       type={type}
-      data={getTotalsByAircraft(data ?? [], type, models ?? [], customFields ?? [])}
+      data={getTotalsByAircraft(flights, type, models, aircrafts, customFields)}
       isLoading={isLoading}
       customFields={customFields ?? []}
     />
