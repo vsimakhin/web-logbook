ee0463130cea2d42db7d0deb00746dc5f4118af2
Vladimir Simakhin <vsimakhin@gmail.com>
2025-12-27 17:18:52 +0100

    missing navigate

--- app/ui/src/components/UIElements/FlightTags.jsx
+++ app/ui/src/components/UIElements/FlightTags.jsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
 // Custom components
 import Select from "./Select";
 import { fetchTags } from "../../util/http/logbook";
 
 export const FlightTags = ({ gsize, id = "tags", label = "Tags", value, handleChange, ...props }) => {
+  const navigate = useNavigate();
 
   const { data: options = [] } = useQuery({
     queryKey: ['logbook', 'tags'],
-    queryFn: ({ signal }) => fetchTags({ signal }),
+    queryFn: ({ signal }) => fetchTags({ signal, navigate }),
     staleTime: 3600000,
     gcTime: 3600000,
   });
