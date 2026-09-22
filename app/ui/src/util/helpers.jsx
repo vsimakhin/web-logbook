4a721505f7b4ea1cef08002e78cce8340ab0e03a
Vladimir Simakhin <vsimakhin@gmail.com>
2026-09-22 11:30:33 +0200

    remove unused functions

--- app/ui/src/util/helpers.jsx
+++ app/ui/src/util/helpers.jsx
@@ -1,5 +1,3 @@
-import dayjs from "dayjs";
-
 // Convert minutes to time format
 export const timeFieldFormat = (minutes, autoFormat = 1, formatZero = false) => {
   if (minutes < 0) return "";
@@ -46,54 +44,8 @@ export const parseTimeToMinutes = (input, autoFormat = 1) => {
   return isNaN(num) ? 0 : Math.max(0, num);
 };
 
-// Convert minutes to HHHH:MM format
-// TODO: Check for delete
-export const convertMinutesToTime = (minutes) => {
-  if (!minutes) return "00:00";
-
-  const hours = String(Math.floor(minutes / 60)).padStart(2, '0');
-  const mins = String(minutes % 60).padStart(2, '0');
-  return `${hours}:${mins}`;
-};
-
-// Convert hours to HHHH:MM format
-// TODO: Check for delete
-export const convertHoursToTime = (hours) => {
-  if (!hours) return "00:00";
-  const totalMinutes = Math.floor(hours * 60);
-  const formattedTime = convertMinutesToTime(totalMinutes);
-  return formattedTime;
-};
-
-// Convert HHHH:MM format back to minutes if needed
-// TODO: Check for delete
-export const convertTimeToMinutes = (time) => {
-  if (!time) return 0;
-  const [hours, mins] = time.split(':').map(Number);
-  return hours * 60 + mins;
-};
-
 export const getValue = (obj, path) => {
   return path.split('.').reduce((acc, key) => (acc ? acc[key] : undefined), obj);
-};
-
-// custom filter function for date range
-export const dateFilterFn = (row, columnId, filterValue) => {
-
-  const rowDate = dayjs(getValue(row.original, columnId), "DD/MM/YYYY");
-  const [startDate, endDate] = filterValue || [];
-
-  // If no filter is applied, return true
-  if (!startDate && !endDate) return true;
-
-  const start = startDate ? new Date(startDate).getTime() : null;
-  const end = endDate ? new Date(new Date(endDate).setHours(23, 59, 59, 999)).getTime() : null; // Include the end of the day
-
-  // Check if the row date is within the selected range
-  const isAfterStart = start !== null ? rowDate >= start : true;
-  const isBeforeEnd = end !== null ? rowDate <= end : true;
-
-  return isAfterStart && isBeforeEnd;
 };
 
 const TIME_FIELDS = [
