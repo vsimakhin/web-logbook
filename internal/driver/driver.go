a160bd55e823580252e8d89ae116db51fd713b5d
Vladimir Simakhin <vsimakhin@gmail.com>
2026-09-24 20:20:58 +0200

    remove stats view and related structures

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -283,7 +283,7 @@ func validateDB(db *sql.DB, engine string) error {
 		}
 
 		// check views
-		views := []*View{logbookView, airportsView, aircraftsView, logbookStatsView, attachmentsView}
+		views := []*View{logbookView, airportsView, aircraftsView, attachmentsView}
 		for _, view := range views {
 			if err := view.initView(db, engine); err != nil {
 				return err
